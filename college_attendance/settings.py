72e113c22af4ed49a52fc01a25216ad210379380
Arindam Garai <fromarindam@yahoo.com>
2026-06-30 19:48:49 +0530

    Fix missing os import

--- college_attendance/settings.py
+++ college_attendance/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%1n6%(!82h2ufkpof6e_+ux5#@(bmv*c*u62oe3!xlpin+74zt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
